4490615309972c0c788b9b9ffbb93c06ac7c9446
Nicolás <nmengualgomez@gmail.com>
2018-05-07 09:33:20 +0200

    Corrected color code

--- StyledComponents.js
+++ StyledComponents.js
@@ -4,7 +4,7 @@ import { Text } from 'react-native'
 export const AlphabetLetter = styled(Text)`
   font-size: 12px;
   font-weight: bold;
-  color: '#007AFF';
+  color: #007AFF;
   text-align: center;
   width: 15px;
   height: 15px;
